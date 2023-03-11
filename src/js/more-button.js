06286cbe09defe16eab2c1dd880b72313ea69b20
Yuriy-risk <111949781+Yuriy-risk@users.noreply.github.com>
2023-03-11 21:57:22 +0200

    init

--- src/js/more-button.js
+++ src/js/more-button.js
@@ -1,13 +1,13 @@
-// export default class MoreButton {
-//   constructor() {
-//     this.button = document.querySelector('.load-more');
-//   }
+export default class MoreButton {
+  constructor() {
+    this.button = document.querySelector('.load-more');
+  }
 
-//   show() {
-//     this.button.classList.remove('is-hidden');
-//   }
+  show() {
+    this.button.classList.remove('is-hidden');
+  }
 
-//   hide() {
-//     this.button.classList.add('is-hidden');
-//   }
-// }
+  hide() {
+    this.button.classList.add('is-hidden');
+  }
+}
